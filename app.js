fb97d7bcd550a7e501de10aae2223aa4f1d21d1c
Jackson Oppenheim <jacksonopp@gmail.com>
2019-10-30 14:50:53 -0500

    added basic schema and resolver

--- app.js
+++ app.js
@@ -1,13 +1,42 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const graphqlHttp = require("express-graphql");
+const { buildSchema } = require("graphql");
 
 //create app
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get("/", (req, res, next) => {
-    res.send("hello")
-})
+app.use("/graphql", graphqlHttp({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `), //points to a graphql schema
+    rootValue: { //points to object that has the resolver functions
+        events: () => {
+            return [
+                'Romantic Cooking',
+                'Sailing',
+                'All-Night-Coding'
+            ]
+        },
+        createEvent: (args) => {
+            const eventName = args.name;
+            return eventName;
+        }
+    },
+    graphiql: true
+}));
 
 app.listen(3000);
